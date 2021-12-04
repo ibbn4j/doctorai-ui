2e265830883a3ebadeab03e8b3abadd18f3fafdf
ibbn4j <irwan.butarbutar@neo4j.com>
2021-12-04 22:02:53 +0700

    initial

---
+++ src/config_example.js
@@ -0,0 +1,14 @@
+// Copy this file to config.js
+const env = {
+    AWS_ACCESS_KEY: '--',
+    AWS_SECRET: '--',
+    AWS_USERID: '--',
+    AWS_REGION: '--',
+
+    LEX_botAliasId: '--',
+    LEX_botId: '--',
+    LEX_localeId: '--',
+    LEX_sessionId: '--'
+}
+
+export default env;
